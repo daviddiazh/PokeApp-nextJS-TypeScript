4298f50b044c071a14f11e09ded510e3829bc796
David Diaz <daviddiazlds1221@gmail.com>
2022-03-05 18:09:07 -0500

    Se genera el endPoint en el Server-side para posteriormente consumirlos en el Client-side

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,17 +1,36 @@
-import type { NextPage } from 'next'
-import { Button } from '@nextui-org/react'
+import type { NextPage, GetStaticProps } from 'next'
 import { Layout } from '../components/layouts'
+import { pokeApi } from '../api';
+import { PokemonListResponse } from '../interfaces/pokemon-list';
 
-const HomePage: NextPage = () => {
+const HomePage: NextPage = (props) => {
+
+  console.log(props)
+
   return (
     <>
       <Layout title='Listado de Pokémons'>
-        <Button color='gradient'>
-          Hola mundo
-        </Button>
+
+        <ul>
+            <li></li>
+        </ul>
+        
       </Layout>
     </>
   )
 }
+
+
+export const getStaticProps: GetStaticProps = async (ctx) => {
+  
+  const { data } = await pokeApi.get<PokemonListResponse>('/pokemon?limit=151')
+
+  return {
+    props: {
+      pokemons: data.results
+    }
+  }
+}
+
 
 export default HomePage
